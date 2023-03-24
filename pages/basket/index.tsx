0ec2efa3fef512bc02bc90cae8270711630aa83b
admrts <alidemirtas.7@icloud.com>
2023-03-24 15:33:37 +0300

    add cart title

--- pages/basket/index.tsx
+++ pages/basket/index.tsx
@@ -1,10 +1,14 @@
 import Basket from "@/Components/Basket";
 import Navbar from "@/Components/Navbar";
 import React from "react";
+import Head from "next/head";
 
 function Index() {
   return (
     <>
+      <Head>
+        <title>Cart</title>
+      </Head>
       <Navbar />
       <Basket />
     </>
